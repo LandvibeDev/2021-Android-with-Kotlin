e515e0634a2c6c1bf0d92a45a98b0066cbd3ac22
JJJoonngg <whdtls3878@gmail.com>
2021-08-19 21:20:34 +0900

    View 구성하기
    - 아이콘 추가
    - main layout 구성

--- ETC-Document/DemoStagram/app/src/main/java/kr/co/jjjoonngg/demostagram/MainActivity.kt
+++ ETC-Document/DemoStagram/app/src/main/java/kr/co/jjjoonngg/demostagram/MainActivity.kt
@@ -3,9 +3,8 @@ package kr.co.jjjoonngg.demostagram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
